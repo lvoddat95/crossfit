52652238262ef68ea1c431365beb969118377f58
toan <deptraimoitoinghien@gmail.com>
2019-05-06 15:35:42 +0700

    update event-name.php
    update event-name.php

--- page-templates/event-name.php
+++ page-templates/event-name.php
@@ -14,137 +14,127 @@
 // Forces full width content layout.
 add_filter('genesis_site_layout', '__genesis_return_full_width_content');
 
-add_action( 'genesis_entry_content', 'crf_entry_content_event_name', 12 );
-function crf_entry_content_event_name() { 
-    
-	$section_event = get_field('section_event');
-	extract($section_event);
+add_action('genesis_entry_content', 'crf_entry_content_event_name', 12);
+function crf_entry_content_event_name()
+{
 
-	if(empty($size))
-		$size= array(300,300);
+    $section_event = get_field('section_event');
+    extract($section_event);
 
-?>
+    if (empty($size))
+        $size = array(689, 498);
 
-<div class="section-event">
-	<div class="container">
-		
-		<div class="row">
-			<div class="col-sm-12 col-xs-12 col-md-12">
-				<div class="wrap1 text-center">
-				<div class="img-wrap event-img1">
-					<?php if ($event_img1) echo '<img src="' . wp_get_attachment_image($event_img1,$size,false); ?>
-				</div>
-				<div class="content-wrap event-content1">
-					<?php if($event_content1) echo esc_html($event_content1); ?>
+    ?>
+    <div class="section-event">
+        <div class="container">
 
-				</div>
-				</div>
-				<div class="event-title"><h2><?php if($event_title) echo esc_html($event_title); ?></h2>
-				</div>
+            <div class="row">
+                <div class="col-sm-12 col-xs-12 col-md-12">
+                    <div class="wrap1">
+                        <div class="img-wrap event-img1 flex-wrap justify-content-center">
+                            <?php if (!empty($event_img1)) echo wp_get_attachment_image($event_img1, array(522,522), false); ?>
+                        </div>
+                        <div class="content-wrap event-content1">
+                            <?php if (!empty($event_content1)) echo apply_filters('crossfit-output-content', $event_content1); ?>
+
+                        </div>
+                    </div>
+                    <div class="event-title"><h2><?php if (!empty($event_title)) echo esc_html($event_title); ?></h2>
+                    </div>
+                    <div class="row wrap1">
+                        <div class="col-lg-6 col-md-6 col1-content">
+                            <div class="event-title1">
+                                <h2><?php if (!empty($event_row_1_title)) echo esc_html($event_row_1_title); ?></h2>
+                            </div>
+                            <div class="wrap-content-col-1">
+                                <?php
+                                if (!empty($event_row_1_group) && is_array($event_row_1_group)) {
+                                    foreach ($event_row_1_group as $key => $value) {
+                                        extract($value);
+                                        ?>
+                                        <h3 class="event-row-1-title">
+                                            <?php if (!empty($event_row_1_title)) echo esc_html($event_row_1_title); ?>
+                                        </h3>
+                                        <div class="event-row-1-content">
+                                            <?php if (!empty($event_row_1_desciption)) echo apply_filters('crossfit-output-content', $event_row_1_desciption); ?>
+                                        </div>
+                                        <?php
+                                    }
+                                }
+                                ?>
+                            </div>
+                        </div>
+                        <div class="col-lg-6 col-md-6 col2-img">
+                            <div class="img-wrap event-img2">
+                                <?php if (!empty($event_row_1_image)) echo wp_get_attachment_image($event_row_1_image, $size, false); ?>
+                            </div>
+                        </div>
+                        <div class="col-lg-12 col-md-12 col-xs-12 last-row1">
+                            <h3><?php if (!empty($event_last_row_1_text)) echo esc_html($event_last_row_1_text); ?></h3>
+                        </div>
+                    </div>
+                    <div class="row wrap2">
+                        <div class="event-title-row-2">
+                            <h2><?php if (!empty($event_title_row_2)) echo esc_html($event_title_row_2); ?></h2>
+                        </div>
+                        <?php
+                        if (!empty($event_row_2_group) && is_array($event_row_2_group)) {
+                            foreach ($event_row_2_group as $key => $value) {
+                                extract($value);
+                                ?>
+                                <div class="event-row-2-desc">
+                                    <h3 class="event-row-2-desc-title"><?php if (!empty($event_row_2_title)) echo esc_html($event_row_2_title); ?></h3>
+                                    <div class="event-row-2-desc-content">
+                                        <?php if (!empty($event_row_2_desciption)) echo apply_filters('crossfit-output-content', $event_row_2_desciption); ?>
+                                    </div>
+                                </div>
+                                <?php
+                            }
+                        }
+                        ?>
+                    </div>
+                    <div class="row wrap3">
+                        <div class="col-md-6 col-lg-6 col-xs-6">
+                            <div class="img-wrap event-img3">
+                                <?php if (!empty($event_img_row_3)) echo wp_get_attachment_image($event_img_row_3, $size, false); ?>
+                            </div>
+                        </div>
+                        <div class="col-md-6 col-lg-6 col-xs-6">
+                            <div class="event-title3">
+                                <h2>
+                                    <?php if (!empty($event_title_row_3)) echo esc_html($event_title_row_3) ?>
+                                </h2>
+                                <div class="event-content3a">
+                                    <?php if (!empty($event_content_right_row3)) echo apply_filters('crossfit-output-content',$event_content_right_row3); ?>
+                                </div>
+                            </div>
+                        </div>
 
 
-				<div class="row wrap1">
-					<div class="col-lg-6 col-md-6 col1-content">
-						<div class="event-title1">
-							<h2><?php if($event_title1) echo esc_html($event_title1); ?></h2>
-						</div>
-						<div class="wrap-content-col-1">
-							<h3 class="title-1a">
-								<?php if($event_title1a) echo esc_html($event_title1a); ?>
-								
-							</h3>
-							<div class="content-1a">
-								<?php if($event_content1a) echo esc_html($event_content1a); ?>
-							</div>
-							<h3 class="title-1b">
-								<?php if($event_title1b) echo esc_html($event_title1b); ?>
-							</h3>
-							<div class="content-1b">
-								<?php if($event_content1b) echo esc_html($event_content1b); ?>
-							</div>
-						</div>
-					
-					</div>
-					<div class="col-lg-6 col-md-6 col2-img">
-						<div class="img-wrap event-img2">
-							<?php if ($event_img2) echo '<img src="' . wp_get_attachment_image($event_img2,$size,false); ?>
-						</div>
-					</div>
-					<div class="last-row1">
-						<h3><?php if($last_row1) echo esc_html($last_row1); ?></h3>
-					</div>
-				</div>
+                    </div>
+                    <div class=" row event-content3b">
+                        <?php if (!empty($event_content_last_row_3)) echo apply_filters('crossfit-output-content',$event_content_last_row_3); ?>
 
-				<div class="row wrap2">
-					<div class="event-title2"><h2>
-						<?php if($event_title2) echo esc_html($event_title2); ?>
-							</h2>
-					</div>
-					<div class="event-2a">
-							<h3 class="event-title2a"><?php if($event_title2a) echo esc_html($event_title2a); ?></h3>
-
-						<?php if($event_content2a) echo esc_html($event_content2a); ?>
-					</div>
-					<div class="event-2b">
-						<h3 class="event-title2b">
-							<?php if($event_title2a) echo esc_html($event_title2a); ?></h3>
-						
-						<?php if($event_content2b) echo esc_html($event_content2b); ?>
-					</div>
-					<div class="event-2c">
-						<h3 class="event-title2c">
-							<?php if($event_title2c) echo esc_html($event_title2c); ?></h3>
-
-						<?php if($event_content2c) echo esc_html($event_content2c); ?>
-					</div>
-
-				</div>
-
-				<div class="row wrap3">
-					<div class="col-md-6 col-lg-6 col-xs-6">
-						<div class="img-wrap event-img3">
-						<?php if ($event_img3) echo '<img src="' . wp_get_attachment_image($event_img3,$size,false); ?>
-						</div>
-					</div>
-					<div class="col-md-6 col-lg-6 col-xs-6">
-						<div class="event-title3">
-							<h2>
-								<?php if($event_title3) echo esc_html($event_title3) ?>
-							</h2>
-							<div class="event-content3a">
-								<?php if($event_content3a) echo esc_html($event_content3a); ?>
-							</div>
-						</div>
-						
-					</div>
-					
-					
-				</div>
-				<div class=" row event-content3b">
-						<?php if($event_content3b) echo esc_html($event_content3b); ?>
-
-					</div>
-				<div class="row event-button">
-					<?php if ($event_button_text): ?>
+                    </div>
+                    <div class="row event-button">
+                        <?php if (!empty($event_button_text)): ?>
                             <a class="btn-event"
-                               href="<?php echo esc_html($event_button_url); ?>"><?php echo esc_html($event_button_text); ?></a>
+                               href="<?php if(!empty($event_button_url)) echo esc_html($event_button_url); ?>"><?php echo esc_html($event_button_text); ?></a>
                         <?php endif; ?>
-				</div>
+                    </div>
 
 
-			</div>
+                </div>
 
 
-		</div>
-	</div>
-	
+            </div>
+        </div>
 
 
-</div>
+    </div>
 
 
-
-
-<?php    
+    <?php
 }
+
 genesis();
