6911a581a4e3b259dc2a008472b6c774e70d8461
toan <deptraimoitoinghien@gmail.com>
2019-05-08 18:50:05 +0700

    update response

--- function-toan.php
+++ function-toan.php
@@ -8,5 +8,5 @@
 add_action( 'wp_enqueue_scripts', 'func_crossfit_enqueue_scripts_styles_2' );
 function func_crossfit_enqueue_scripts_styles_2() {
 	wp_enqueue_style('toan-css',get_stylesheet_directory_uri().'/assets/css/toan-css.css');
-    wp_enqueue_style('toan-css',get_stylesheet_directory_uri().'/assets/css/toan-response.css');
+    wp_enqueue_style('toan-response-css',get_stylesheet_directory_uri().'/assets/css/toan-response.css');
 }
