9ecc19fe9f260b531346b38e9876c9c5476cb734
Dat Lv <lvoddat95@yahoo.com>
2019-05-12 18:11:42 +0700

    dat lv

--- function-toan.php
+++ function-toan.php
@@ -9,4 +9,5 @@ add_action( 'wp_enqueue_scripts', 'func_crossfit_enqueue_scripts_styles_2' );
 function func_crossfit_enqueue_scripts_styles_2() {
 	wp_enqueue_style('toan-css',get_stylesheet_directory_uri().'/assets/css/toan-css.css');
     wp_enqueue_style('toan-response-css',get_stylesheet_directory_uri().'/assets/css/toan-response.css');
+    wp_enqueue_style('responsive-css',get_stylesheet_directory_uri().'/assets/css/responsive.css');
 }
